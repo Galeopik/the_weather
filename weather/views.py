5f3350e811887f6540602a2dc80c2fce7a0526df
Gleb Belousov <galeopik@gmail.com>
2026-07-27 00:22:42 +0300

    Add new files

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,11 @@
+import requests
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=a026f6573c3305150058c0d39784dc07'
+    city = "Moscow"
+    r = requests.get(url.format(city))
+    print(r.text)
+    return render(request, 'index.html')
