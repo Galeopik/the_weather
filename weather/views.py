2f040a491c18a26c09282ac281fffa248224eed6
Gleb Belousov <galeopik@gmail.com>
2026-08-02 01:25:26 +0300

    Response working

--- weather/views.py
+++ weather/views.py
@@ -5,7 +5,16 @@ from django.shortcuts import render
 
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=a026f6573c3305150058c0d39784dc07'
-    city = "Moscow"
-    r = requests.get(url.format(city))
-    print(r.text)
-    return render(request, 'index.html')
+    city = "Japan"
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city': city,
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+    }
+
+    context = {'city_weather': city_weather}
+
+    return render(request, 'index.html', context)
