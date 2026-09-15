94ce0b79fd38d0b918c1b1dd65a947dcea230f61
Gleb Belousov <galeopik@gmail.com>
2026-09-16 01:26:17 +0300

    Local

--- weather/views.py
+++ weather/views.py
@@ -1,20 +1,26 @@
+import os
 import requests
 
 from django.shortcuts import render
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def index(request):
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=a026f6573c3305150058c0d39784dc07'
-    city = "Japan"
-    r = requests.get(url.format(city)).json()
+    weather_data = None
+    city = request.GET.get('city')
+    if city:
+        api_key = os.getenv('API_KEY', '')
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}&lang=ru'
+        response = requests.get(url).json()
 
-    city_weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-    }
+        if response.get('cod') == 200:
+            weather_data = {
+                'city':city.title(),
+                'temp':response['main']['temp'],
+                'description':response['weather'][0]['description'].title(),
+                'icon': response['weather'][0]['icon'],
+            }
 
-    context = {'city_weather': city_weather}
-
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', {'weather': weather_data})
